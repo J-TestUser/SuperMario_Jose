9c42ee74ee774741283402a37e71adb1dd241b3e
J-TestUser <jestrada@ctsformacion.com>
2026-04-17 12:41:47 +0200

    EnemyMovementUpdate

--- Assets/Scripts/Goomba.cs
+++ Assets/Scripts/Goomba.cs
@@ -20,6 +20,13 @@ public class Goomba : MonoBehaviour
     private int _goombaHealth =3;
     private Slider _goombaHealthSlider;
 
+    public Transform[] patrolPoints;
+    public int patrolIndex = 0;
+
+    private Transform playerPosition;
+    public float detectionRange = 5;
+    public float attackRange = 1;
+
     
 
     void Awake()
@@ -30,6 +37,8 @@ public class Goomba : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
         _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         _goombaHealthSlider = GetComponentInChildren<Slider>();
+
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -47,19 +56,77 @@ public class Goomba : MonoBehaviour
 
     void FixedUpdate()
     {
+        float distanceToPlayer = Vector3.Distance(playerPosition.position, transform.position);
+
+        if(distanceToPlayer > detectionRange)
+        {
+            Patrol();
+        }
+        else if(distanceToPlayer < detectionRange && distanceToPlayer > attackRange)
+        {
+            FollowPlayer();
+        }
+        else if (distanceToPlayer < attackRange)
+        {
+            Attack();
+        }
+    }
+
+    void Patrol()
+    {
+        float distanceToPoint = Vector3.Distance(transform.position,patrolPoints[patrolIndex].position);
+        if (distanceToPoint < 1f)
+        {
+            if (patrolIndex == 0)
+            {
+                patrolIndex = 1;
+            }   
+            else
+            {
+                patrolIndex = 0;
+            }
+        }
+        Vector3 moveDirection = patrolPoints[patrolIndex].position - transform.position;
+        Movement(moveDirection);
+    }
+    void FollowPlayer()
+    {
+        Vector3 moveDirection = playerPosition.position - transform.position;
+        Movement(moveDirection);
+    }
+
+    void Movement(Vector3 moveDirection)
+    {
+        if (moveDirection.x < 0)
+        {
+            direction = -1;
+            transform.rotation = Quaternion.Euler(0,180,0);
+        }
+        else if (moveDirection.x > 0)
+        {
+            direction = 1;
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+
         _rigidBody2D.linearVelocity = new Vector2(direction * movementSpeed, _rigidBody2D.linearVelocity.y);
 
         animator.SetBool("Is Walking", true);
     }
+    void Attack()
+        {
+            direction = 0;
+
+        Debug.Log("Atacando");
+        }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Tuberias") || collision.gameObject.layer == 7)
+        /*if(collision.gameObject.CompareTag("Tuberias") || collision.gameObject.layer == 7)
         {
             //Esto hacec lo mismo que la linea de abajo pero de forma mas intuitiva
             //direction = direction * -1;
             direction *= -1;
-        }
+        }*/
 
         if(collision.gameObject.CompareTag("Player"))
         {
